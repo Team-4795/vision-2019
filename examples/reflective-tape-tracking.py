05d066f8cc7fefe17c08976569d4a2ef29160228
Namr <amrmoussa100@gmail.com>
2019-01-06 13:25:51 -0500

    Added contour detection and position identification

--- examples/reflective-tape-tracking.py
+++ examples/reflective-tape-tracking.py
@@ -17,6 +17,50 @@ maskFrame = cv2.inRange(hsvFrame, lowerHSVBound, upperHSVBound)
 kernel = np.ones((5,5),np.uint8)
 maskFrame = cv2.morphologyEx(maskFrame, cv2.MORPH_OPEN, kernel)
 
+#get contours of the detected tape
+edgeFrame = cv2.Canny(maskFrame,100,200)
+contourFrame, contours, hierarchy = cv2.findContours(edgeFrame,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+#iterate over all found contours and find the two largest
+largestSize = -1
+secondLargestSize = -1
+index = 0
+tape1 = []
+tape2 = []
+
+#get actual hierarchy from within the bloated datatype
+hierarchy = hierarchy[0]
+
+for contour in contours:
+	if hierarchy[index, 3] >= 0: #this removes contour douplicates caused by the canny edge detector
+		size = cv2.contourArea(contour)
+		if size > largestSize:
+			tape1 = contour
+			largestSize = size
+	index += 1
+	
+index = 0
+for contour in contours:
+	if hierarchy[index, 3] >= 0:
+		size = cv2.contourArea(contour)
+		if size > secondLargestSize and size < largestSize:
+			tape2 = contour
+			secondLargestSize = size
+	index += 1
+
+#find the position of the center of these contours and display them on the original frame
+M1 = cv2.moments(tape1)
+M2 = cv2.moments(tape2)
+
+x1 = int(M1['m10']/M1['m00'])
+y1 = int(M1['m01']/M1['m00'])
+
+x2 = int(M2['m10']/M2['m00'])
+y2 = int(M2['m01']/M2['m00'])
+
+cv2.circle(testFrame, (x1, y1), 3, (255, 0, 0), -1)
+cv2.circle(testFrame, (x2, y2), 3, (255, 0, 0), -1)
+
 #display results
 cv2.imshow("testFrame", testFrame)
 #enabling this can be useful for using a color picker to find exactly what range of HSV
